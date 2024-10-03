55c4dd2504f6558b221a4c93549a64f9adadc71e
Andreia <andreia.machion@saojudas.br>
2024-10-03 11:51:29 -0300

    backend criado

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+//get http://localhost:3000/oi
+app.get('/oi', (req, res) => {
+    res.send('oi')
+})
+
+let filmes = [
+    {
+        titulo: "Oppenheimer",
+        sinopse: "O físico J. Robert Oppenheimer trabalha com uma equipe de cientistas durante o Projeto Manhattan, levando ao desenvolvimento da bomba atômica."
+    },
+    {
+        titulo: "Divertidamente 2",
+        sinopse: "Com um salto temporal, Riley se encontra mais velha, passando pela tão temida adolescência. Junto com o amadurecimento, a sala de controle também está passando por uma adaptação para dar lugar a algo totalmente inesperado: novas emoções."
+    }
+]
+
+app.get('/filmes', (req, res) => {
+    res.json(filmes)
+})
+
+app.post('/filmes', (req, res) => {
+    //capturar as informações enviadas
+    const titulo = req.body.titulo
+    const sinopse = req.body.sinopse
+    //montar um objeto json filme com as informações capturadas
+    const novo_filme = {titulo: titulo, sinopse: sinopse}
+    //acrescentar o novo filme à base
+    filmes.push(novo_filme)
+    //para ilustrar, mostrar a base atualizada
+    res.json(filmes)
+})
+
+app.listen (3000, () => console.log("server up & running"))
