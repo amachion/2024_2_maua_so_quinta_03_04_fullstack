ae9ace184bc09b74509c2a74e62e9671e0e03dcf
Andreia <andreia.machion@saojudas.br>
2024-10-10 12:33:43 -0300

    até form para frontend

--- front/js/script.js
+++ front/js/script.js
@@ -5,5 +5,14 @@ const filmesEndpoint = '/filmes'
 async function obterFilmes() {
     const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
     const filmes = (await axios.get(URLcompleta)).data
-    console.log(filmes)
+    
+    let tabela = document.querySelector('.filmes')
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+    for (let filme of filmes) {
+        let linha = corpoTabela.insertRow(0)
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
+    }
 }
