80278935d2a4279680ffdd41176fa0d07d22971a
Andreia <andreia.machion@saojudas.br>
2024-10-17 11:02:10 -0300

    cadastro de filmes com validação

--- front/js/script.js
+++ front/js/script.js
@@ -1,18 +1,42 @@
 const protocolo = 'http://'
 const baseURL = 'localhost:3000'
 const filmesEndpoint = '/filmes'
-
-async function obterFilmes() {
-    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
-    const filmes = (await axios.get(URLcompleta)).data
-    
+function exibirFilmes (filmes) {
     let tabela = document.querySelector('.filmes')
     let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+    corpoTabela.innerHTML = ""
     for (let filme of filmes) {
         let linha = corpoTabela.insertRow(0)
         let celulaTitulo = linha.insertCell(0)
         let celulaSinopse = linha.insertCell(1)
         celulaTitulo.innerHTML = filme.titulo
         celulaSinopse.innerHTML = filme.sinopse
+    }
+}
+async function obterFilmes() {
+    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
+    const filmes = (await axios.get(URLcompleta)).data
+    exibirFilmes(filmes)
+}
+async function cadastrarFilme() {
+    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
+    let tituloInput = document.querySelector('#tituloInput')
+    let sinopseInput = document.querySelector("#sinopseInput")
+    let titulo = tituloInput.value
+    let sinopse = sinopseInput.value
+    tituloInput.value = ""
+    sinopseInput.value = ""
+    if (titulo && sinopse) {
+        const filmes = (await axios.post(URLcompleta, {titulo, sinopse})).data
+        exibirFilmes(filmes)
+    }
+    else {
+        let alert = document.querySelector('.alert')
+        alert.classList.add('show')
+        alert.classList.remove('d-none')
+        setTimeout(() => {
+            alert.classList.remove('show')
+            alert.classList.add('d-none')
+        }, 2000)
     }
 }
